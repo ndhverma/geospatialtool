fbf0034fe49e255e1758e54220e72707c8e8cb2e
Nidhi Verma <43364419+ndhverma@users.noreply.github.com>
2023-03-17 17:19:14 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,9 +13,12 @@ st.sidebar.info(
 st.sidebar.title("Contact")
 st.sidebar.info(
     """
-    Qiusheng Wu: <https://wetlands.io>
-    [GitHub](https://github.com/giswqs) | [Twitter](https://twitter.com/giswqs) | [YouTube](https://www.youtube.com/c/QiushengWu) | [LinkedIn](https://www.linkedin.com/in/qiushengwu)
+    Nidhi Verma: <https://indianspacemap.in/home/>
     """
+    # """
+   # Nidhi Verma: <https://indianspacemap.in/home/>
+    #[GitHub](https://github.com/giswqs) | [Twitter](https://twitter.com/giswqs) | [YouTube](https://www.youtube.com/c/QiushengWu) | [LinkedIn](https://www.linkedin.com/in/qiushengwu)
+    #"""
 )
 
 # Customize page title
